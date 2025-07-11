7b0e85b5267d7af50aada7ee227f0cd3f1a124a6
AdonadoMolinares <donadomolinares@gmail.com>
2025-07-11 11:02:20 -0500

    commit

--- src/config/global.js
+++ src/config/global.js
@@ -130,7 +130,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF01_63110190_DU.pdf',
       },
       {
         icono: 'fas fa-download',
